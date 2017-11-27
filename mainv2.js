bf2a4c5c85546bc6426e0d8ef3e2081ac77a68ed
unknown <benjah88@gmail.com>
2017-11-27 14:19:59 -0800

    chalk added

--- mainv2.js
+++ mainv2.js
@@ -1,9 +1,10 @@
 var inquirer = require("inquirer");
 const chalk = require("chalk");
 var gl = 10;
-var movie = ['top gun', 'blade runner', 'star wars', 'jurassic park', 'eternal sunshine of the spotless mind', 
-'little miss sunshine', 'pulp fiction', 'the wolf of wall street', 'saving private ryan',
-'aliens', 'predator', 'avatar', 'inception', 'the dark knight', 'shrek', 'jaws', 'thor'];
+var movie = ['top gun', 'blade runner', 'star wars', 'jurassic park', 'eternal sunshine of the spotless mind',
+  'little miss sunshine', 'pulp fiction', 'the wolf of wall street', 'saving private ryan',
+  'aliens', 'predator', 'avatar', 'inception', 'the dark knight', 'shrek', 'jaws', 'thor'
+];
 var Word = require("./word.js");
 var mysql = require('mysql');
 var letsArr = [];
@@ -13,346 +14,334 @@ var winflag = false;
 var lossflag = false;
 var namey;
 var pwy;
-var win ;
-var loss ;
-var winy ;
-var lossy ;
+var win;
+var loss;
+var winy;
+var lossy;
 var con = mysql.createConnection({
   host: "localhost",
   user: "Ben",
   password: "bgrant88",
   database: "hangmandb"
-  
+
 });
 
 ask();
 
-function ask(){
+function ask() {
 
-console.log("\nWelcome to Movie Hangman Node Madness!!!\n");
+  console.log("\nWelcome to Movie Hangman Node Madness!!!\n");
 
-inquirer
-  .prompt([
-    {
+  inquirer
+    .prompt([{
       type: "confirm",
       message: "Do already have an account?",
       name: "conf",
       default: true
-    }
-    ])
-  .then(function(acc) {
+    }])
+    .then(function(acc) {
 
-if (acc.conf){
+      if (acc.conf) {
 
-  signin();
+        signin();
+      } 
+
+      else {
+
+        create();
+      }
+
+    });
+
 }
 
-else {
+function create() {
 
-  create();
-}
-
-  });
-
-}
-
-function create(){
-
-	inquirer
-  .prompt([
-    {
+  inquirer
+    .prompt([{
       type: "input",
       message: "Please create a username:",
       name: "user"
-    },
-    {
+    }, {
       type: "password",
       message: "Plese create a password:",
       name: "pws",
       hidden: true,
       mask: "*"
-    }
-    ])
-  .then(function(sign) {
+    }])
+    .then(function(sign) {
 
-con.connect(function(err) {
-  if (err) throw err;
+      con.connect(function(err) {
+        if (err) throw err;
 
-   var sql = "INSERT INTO data (username, password, wins, losses) VALUES ?";
-   values = [
+        var sql = "INSERT INTO data (username, password, wins, losses) VALUES ?";
+        values = [
 
-   [sign.user, sign.pws, 0, 0]
+          [sign.user, sign.pws, 0, 0]
 
-   ];
+        ];
 
-  con.query(sql, [values], function (err, result) {
-    if (err) throw err;
+        con.query(sql, [values], function(err, result) {
+          if (err) throw err;
 
-  });
+        });
 
-  });
-  signin();
+      });
+      signin();
 
-});
+    });
 
 }
 
-function signin(){
+function signin() {
 
-inquirer
-  .prompt([
-    {
+  inquirer
+    .prompt([{
       type: "input",
       message: "Please sign in with your username:",
       name: "user2"
-    },
-    {
+    }, {
       type: "password",
       message: "Please sign in with your password:",
       name: "pws2",
       hidden: true,
       mask: "*"
-    }
-    ])
+    }])
 
-.then(function(sign2) {
+  .then(function(sign2) {
 
-  con.query("SELECT * FROM data", function (err, result, fields) {
-    if (err) throw err;
+    con.query("SELECT * FROM data", function(err, result, fields) {
+      if (err) throw err;
 
-for (var i = 0; i < result.length; i++) {
+      for (var i = 0; i < result.length; i++) {
 
-     if (sign2.user2 === result[i].username && sign2.pws2 === result[i].password){
+        if (sign2.user2 === result[i].username && sign2.pws2 === result[i].password) {
 
-  console.log("\nWelcome " + sign2.user2);
-  console.log("\nYour Wins: " + result[i].wins);
-  console.log("\nYour Losses: " + result[i].losses);
-  win = result[i].wins;
-  loss = result[i].losses;
-  winy = win;
-  lossy = loss;
-  namey = sign2.user2;
-  pwy = sign2.pws2;
-  i = result.length;
+          console.log("\nWelcome " + sign2.user2);
+          console.log("\nYour Wins: " + result[i].wins);
+          console.log("\nYour Losses: " + result[i].losses);
+          win = result[i].wins;
+          loss = result[i].losses;
+          winy = win;
+          lossy = loss;
+          namey = sign2.user2;
+          pwy = sign2.pws2;
+          i = result.length;
 
-    restart();
-	  start();
-	  guess();
+          restart();
+          start();
+          guess();
 
-       return false;
+          return false;
 
-}
+        }
 
-  };
+      };
 
-  console.log("Sorry, wrong username/password. Try again:\n");
-  signin();
-  return false;
+      console.log("Sorry, wrong username/password. Try again:\n");
+      signin();
+      return false;
+
+    });
 
   });
 
-});
+}
+
+function start() {
+
+  console.log("\nMovie Name: \n");
+  console.log(mov.letsIn.join(" ") + "\n");
+}
+
+function restart() {
+
+  letsArr = [];
+  gl = 10;
+  mov = new Word(randy());
+  mov.letsIn = [];
+  mov.display();
+  corrflag = false;
+  winflag = false;
+  lossflag = false;
 
 }
 
-function start (){
+function randy() {
 
-console.log("\nMovie Name: \n");
-console.log(mov.letsIn.join(" ") + "\n");
+  return movie[Math.floor(Math.random() * movie.length)];
 }
 
-function restart (){
+function agains() {
+  inquirer.prompt([{
+    type: "confirm",
+    message: "Play again?",
+    name: "conf",
+    default: true
 
- letsArr = [];
- gl = 10;
- mov = new Word (randy());
- mov.letsIn = [];
- mov.display();
- corrflag = false;
- winflag = false;
- lossflag = false;
+  }]).then(function(again) {
 
-}
+    if (again.conf) {
 
-function randy (){
+      restart();
+      start();
+      guess();
+      return false;
+    } 
 
-	return movie[Math.floor(Math.random() * movie.length)];
-}
+    else {
 
-function agains(){
-inquirer.prompt([
-{
- 	  type: "confirm",
-      message: "Play again?",
-      name: "conf",
-      default: true
-
+      console.log("\nBYE " + namey + "!!\n");
+      con.end();
+      return false;
     }
-	]).then(function(again) {
 
-		if(again.conf){
-
-			restart();
-			start();
-			guess();
-			return false;
-		}
-
-		else{
-
-			console.log("\nBYE " + namey + "!!\n" );
-			con.end();
-			return false;
-		}
-
-	})
+  })
 
 }
 
-			function answer(){
+function answer() {
 
-			for (var i = 0; i < mov.movie.length; i++) {
-	
-			if (mov.movie[i] !== mov.letsIn[i]){
-				return false;
-			}
-		}
-			return true;
+  for (var i = 0; i < mov.movie.length; i++) {
+
+    if (mov.movie[i] !== mov.letsIn[i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
-	function correct(){
+function correct() {
 
 
-	console.log(chalk.green("\nCORRECT!\n"));
-	console.log(mov.letsIn.join(" ") + "\n");
-	guess();
+  console.log(chalk.green("\nCORRECT!\n"));
+  console.log(mov.letsIn.join(" ") + "\n");
+  guess();
 }
-	
-function wrong(){
 
-console.log(chalk.red("\nINCORRECT!"));
-	
-	gl = gl -1;
+function wrong() {
 
-	if (gl === 0){
+  console.log(chalk.red("\nINCORRECT!"));
+
+  gl = gl - 1;
+
+  if (gl === 0) {
     lossflag = true;
-     loss++;
+    loss++;
 
-    var sql = "UPDATE data SET losses = '"+ loss+ "' WHERE losses ='" + lossy + "'";
-     
-        con.query(sql, function (err, result) {
-   if (err) throw err;
-  });
+    var sql = "UPDATE data SET losses = '" + loss + "' WHERE losses ='" + lossy + "'";
+
+    con.query(sql, function(err, result) {
+      if (err) throw err;
+    });
 
     lossy++;
-		console.log("\nYOU LOST!");
-		console.log("\nThe movie was: " + mov.movie + "\n");
+    console.log("\nYOU LOST!");
+    console.log("\nThe movie was: " + mov.movie + "\n");
 
-  con.query("SELECT * FROM data", function (err, result, fields) {
-    if (err) throw err;
+    con.query("SELECT * FROM data", function(err, result, fields) {
+      if (err) throw err;
 
-for (var i = 0; i < result.length; i++) {
+      for (var i = 0; i < result.length; i++) {
 
-     if (namey === result[i].username && pwy === result[i].password){
+        if (namey === result[i].username && pwy === result[i].password) {
 
-  console.log("Your Wins: " + result[i].wins);
-  console.log("\nYour Losses: " + result[i].losses+ "\n");
-  return false;
+          console.log("Your Wins: " + result[i].wins);
+          console.log("\nYour Losses: " + result[i].losses + "\n");
+          return false;
+        }
+      }
+    });
+
+    setTimeout(function() {
+
+      restart();
+      agains();
+      return false;
+
+    }, 500);
+  } 
+
+  else if (lossflag === false) {
+
+    console.log("\nGuesses Left: " + gl + "\n");
+    console.log("Letters Guessed " + letsArr.join(" ") + "\n");
+    console.log(mov.letsIn.join(" ") + "\n");
+    guess();
+  }
 }
-}
-});
 
-  setTimeout(function () {
+function guess() {
 
-		restart();
-		agains();
-		return false;
+  inquirer.prompt([{
+    name: "let",
+    message: "Guess a letter:",
+    validate: function(lets) {
+      if (lets.length === 1 && letsArr.indexOf(lets.toLowerCase()) === -1 && isNaN(lets)) {
+        return true;
+      }
+      return false;
+    },
 
-    }, 500); 
-}
+  }]).then(function(guess1) {
 
-	else if (lossflag === false){
+    corrflag = false;
+    for (var i = 0; i < mov.movie.length; i++) {
 
-	console.log("\nGuesses Left: " + gl + "\n");
-	console.log("Letters Guessed " + letsArr.join(" ") + "\n");
-	console.log(mov.letsIn.join(" ") + "\n");
-	guess();
-	}
-}
+      if (guess1.let.toLowerCase() === mov.movie.charAt([i])) {
+        corrflag = true;
+        mov.letsIn[i] = mov.movie[i];
 
-function guess (){
 
-inquirer.prompt([
-{
- 		name: "let",
-        message: "Guess a letter:",
-        validate: function(lets) {
-	        if (lets.length ===1 && letsArr.indexOf(lets.toLowerCase()) === -1 && isNaN(lets)) {
-	          return true;
-	        }
-	          return false;
-	      },		      
+        if (answer()) {
+          corrflag = false;
+          winflag = true;
+          win++;
+          var sql = "UPDATE data SET wins = '" + win + "' WHERE wins ='" + winy + "'";
+
+          con.query(sql, function(err, result) {
+            if (err) throw err;
+          });
+
+          winy++;
+          console.log("\nYou won!!\n");
+          console.log("the movie was: " + mov.movie + "\n");
+
+          con.query("SELECT * FROM data", function(err, result, fields) {
+            if (err) throw err;
+
+            for (var i = 0; i < result.length; i++) {
+
+              if (namey === result[i].username && pwy === result[i].password) {
+
+                console.log("Your Wins: " + result[i].wins);
+                console.log("\nYour Losses: " + result[i].losses + "\n");
+                return false;
+              }
+            }
+          });
+
+          setTimeout(function() {
+
+            restart();
+            agains();
+            return false;
+
+          }, 500);
+        }
+
+      }
 
     }
-	]).then(function(guess1) {
+    if (corrflag === true) {
+      letsArr.push(guess1.let.toLowerCase());
+      correct();
+      return false;
+    }
 
-			corrflag = false;
-	for (var i = 0; i < mov.movie.length; i++) {
-
-		if (guess1.let.toLowerCase() === mov.movie.charAt([i])){
-		corrflag = true;
-		mov.letsIn[i] = mov.movie[i];
-		
-
-			if (answer()){
-        corrflag = false;
-        winflag = true;
-        win++;
-        var sql = "UPDATE data SET wins = '"+ win+ "' WHERE wins ='" + winy + "'";
-     
-        con.query(sql, function (err, result) {
-   if (err) throw err;
-  });
-
-        winy++;
-	console.log("\nYou won!!\n");
-	console.log("the movie was: " + mov.movie + "\n");
-
-  con.query("SELECT * FROM data", function (err, result, fields) {
-    if (err) throw err;
-
-for (var i = 0; i < result.length; i++) {
-
-     if (namey === result[i].username && pwy === result[i].password){
-
-  console.log("Your Wins: " + result[i].wins);
-  console.log("\nYour Losses: " + result[i].losses + "\n");
-  return false;
-}
-}
-});
-
-  setTimeout(function () {
-
-    restart();
-    agains();
-    return false;
-
-    }, 500); 
-	}
-		
-	}
-
-}	
-	if (corrflag === true) {
-  letsArr.push(guess1.let.toLowerCase());
-	correct();
-	return false;
-	}
-
-if (winflag === false){
-	letsArr.push(guess1.let.toLowerCase());
-	wrong();
-}
-	})
+    if (winflag === false) {
+      letsArr.push(guess1.let.toLowerCase());
+      wrong();
+    }
+  })
 }
